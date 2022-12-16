8fdc68a28b378c341061c8acdb1174ea564f2c25
Ravinder Bindra <120709825+ravinder-bindra@users.noreply.github.com>
2022-12-16 14:46:16 +0530

    Create #sine and cosine values.py

---
+++ #sine and cosine values.py
@@ -0,0 +1,7 @@
+#sine and cosine values
+import math
+for i in range(0,345,15):
+    print('The sine value of ',i,':',round(math.sin(i),4))
+    print('The cosine value of ',i,':',round(math.cos(i),4))
+    
+
